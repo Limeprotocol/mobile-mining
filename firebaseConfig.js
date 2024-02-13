53bf9215f15d891d134166cbc18bc9c944007b2b
Pietro Ciattaglia <pietro.ciattaglia@gmail.com>
2024-02-13 10:53:05 +0100

    added pwa support and firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHtatyVzbmil7AFq2UjO9c4w9HW5G8v2Q",
+  authDomain: "progetto-recupero-4ff23.firebaseapp.com",
+  projectId: "progetto-recupero-4ff23",
+  storageBucket: "progetto-recupero-4ff23.appspot.com",
+  messagingSenderId: "754767668367",
+  appId: "1:754767668367:web:761ec632202698a4a9fcfa",
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
